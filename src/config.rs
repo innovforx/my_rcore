1b5121849d2c506b4808b9fa1ffef7aa89a525f5
forx <forx.creation@qq.com>
2022-06-01 05:45:53 -0700

    add kernel-stack/heap trapline/trap-context ...

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,18 @@
 pub const USER_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
-pub const MAX_APP_NUM: usize = 4;
-pub const APP_BASE_ADDRESS: usize = 0x80400000;
-pub const APP_SIZE_LIMIT: usize = 0x20000;
+pub const KERNEL_HEAP_SIZE: usize = 0x30_000;
+pub const MEMORN_END:usize = 0x8080_0000;
+pub const PAGE_SIZE:usize = 0x1_000;
+pub const PAGE_SIZE_BITS : usize = 12usize;
+
+pub const TRAMPLINE : usize = usize::MAX - (PAGE_SIZE - 1);
+pub const TRAP_CONTEXT :usize = TRAMPLINE - PAGE_SIZE;
+
+pub fn kernel_stack_position(appid : usize) -> (usize,usize){
+    let top = TRAMPLINE - appid*(KERNEL_STACK_SIZE + PAGE_SIZE);
+    let bottom = top - KERNEL_STACK_SIZE;
+    (bottom,top)
+}
+
 
 pub use crate::board::CLOCK_FREQ;
